d39f8008e58f35d60eafcc977c5e45c47ce7e498
Elad Karako <eladkarako@gmail.com>
2015-01-22 00:28:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ setlocale(LC_ALL, 'he_IL.UTF-8');
 mb_internal_encoding('UTF-8');
 header('Content-Type: text/html; charset=utf-8', true, 200);
 ?><!DOCTYPE html>
-<html lang="en-US" style="display: none;">
+<html lang="en-US">
 <head>
   <meta charset="utf-8"/>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-  <script defer="defer" src="js.js"></script>
+  <script defer="defer" src="loadjs.js"></script>
 </head>
 <body></body>
 </html>
