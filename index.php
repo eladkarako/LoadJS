5b1933ab13a9c032942968b2e59bb219fcbf4cdb
Elad Karako <eladkarako@gmail.com>
2015-01-22 00:26:55 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+@ini_set('short_open_tag', 'On');    //enable (?= ?) (change '(' to '>' and ')' to '>')
+
+date_default_timezone_set("Asia/Jerusalem");
+setlocale(LC_ALL, 'he_IL.UTF-8');
+mb_internal_encoding('UTF-8');
+header('Content-Type: text/html; charset=utf-8', true, 200);
+?><!DOCTYPE html>
+<html lang="en-US" style="display: none;">
+<head>
+  <meta charset="utf-8"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+  <script defer="defer" src="js.js"></script>
+</head>
+<body></body>
+</html>
